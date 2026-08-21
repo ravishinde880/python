6905af21d78cbdbe5cef060a2e875c02cf169285
Raviraj Shinde <rkshinde880@gmail.com>
2026-08-21 20:14:36 +0530

    Delete Day3.py

--- Day3.py
+++
@@ -1,9 +0,0 @@
-num = int(input("enter the value of num: "))
-if (num > 0):
-    print("number is negative.")
-elif (num == 0):
-    print("number is zero.")
-elif (num == 0):
-    print("number is special")
-else:
-    print("number is positive.")
